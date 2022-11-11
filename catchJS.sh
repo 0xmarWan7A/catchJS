2f8913642a53af9108209bb36d6179aad2f93177
Marwan 7assaAn <69312897+0xmarWan7A@users.noreply.github.com>
2022-11-11 16:30:18 +0200

    Add files via upload

---
+++ catchJS.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="32"
+BOLDGREEN="\e[1;${GREEN}m"
+ENDCOLOR="\e[0m"
+
+clear
+
+echo -e "${BOLDGREEN}"
+
+echo "   #########################################################################"
+echo "   #                      Strating Extracting JS Juciy Data                #"
+echo "   #########################################################################"
+
+#function1
+gf_pattern(){
+
+for i in $(cat jsfiles.txt)
+    do
+
+    done
+
+}
+
+
+
+echo -e "${ENDCOLOR}"
